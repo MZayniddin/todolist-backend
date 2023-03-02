679070fac6d84771f4f51163d56a7febc094b926
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-02 19:55:59 +0500

    added upload-file middleware

--- routes/img-upload.js
+++ routes/img-upload.js
@@ -1,21 +1,10 @@
-const multer = require("multer");
-const path = require("path");
 const { Router } = require("express");
 
 // INIT ROUTER
 const router = Router();
 
-// CONFIG MULTER STORAGE
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({ storage: storage });
+// IMPORT UPLOAD MIDDLEWARE
+const upload = require("../middleware/img-upload-middleware");
 
 // IMPORT CONTROLLER
 const uploadCtr = require("../controllers/img-upload-controller");
